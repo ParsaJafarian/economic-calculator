751b6668af660a770c0716e5f2fad8fcf1259431
ParsaJafarian <108084316+Parsilius@users.noreply.github.com>
2023-09-29 22:07:14 -0400

    Create root & insert InputSection

---
+++ src/main/java/com/compound_calculator/Graph.java
@@ -0,0 +1,2 @@
+package com.compound_calculator;public class Graph {
+}
--- src/main/java/com/compound_calculator/Main.java
+++ src/main/java/com/compound_calculator/Main.java
@@ -3,6 +3,7 @@ package com.compound_calculator;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,14 +11,15 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("index.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        BorderPane root = new BorderPane();
+        root.setLeft(new InputSection());
+        Scene scene = new Scene(root, 320, 240);
         stage.setTitle("Compound Interest Calculator");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }

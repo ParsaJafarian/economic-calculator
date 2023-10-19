d3e180a56b2279ad6523268a2b537402f0a4ed36
Parsa Jafarian <2234400@edu.vaniercollege.qc.ca>
2023-10-19 17:42:09 -0400

    Comment everything out because of errors until fixed

--- src/main/java/com/compound_calculator/Graph.java
+++ src/main/java/com/compound_calculator/Graph.java
@@ -1,24 +1,26 @@
-package com.compound_calculator;
-import javafx.scene.chart.AreaChart;
-import javafx.scene.chart.NumberAxis;
-
-public class Graph extends AreaChart<Double, Double>{
-
-    public Graph(){
-
-        //Defining the X axis
-        NumberAxis xAxis = new NumberAxis();
-        xAxis.setLabel("Time (years)");
-        //Defining the y Axis
-        NumberAxis yAxis = new NumberAxis(0, 15, 2.5);
-        yAxis.setLabel("Capital ($)");
-        super(xAxis, yAxis);
-        System.out.println("Hello there, the angel from my nightmare.");
-
-        //Changes ----add changes to stage ------> commit them -----> push them to github
-    }
-
-    public void setData(double[] data) {
-
-    }
-}
+//package com.compound_calculator;
+//import javafx.scene.chart.AreaChart;
+//import javafx.scene.chart.Axis;
+//import javafx.scene.chart.NumberAxis;
+//
+//public class Graph extends AreaChart<Double, Double>{
+//
+//    public Graph(){
+//        super(new NumberAxis(), new NumberAxis());
+//        NumberAxis axis = new NumberAxis();
+//        //Defining the X axis
+//        NumberAxis xAxis = new NumberAxis();
+//        xAxis.setLabel("Time (years)");
+//        //Defining the y Axis
+//        NumberAxis yAxis = new NumberAxis(0, 15, 2.5);
+//        yAxis.setLabel("Capital ($)");
+//
+//        System.out.println("Hello there, the angel from my nightmare.");
+//
+//        //Changes ----add changes to stage ------> commit them -----> push them to github
+//    }
+//
+//    public void setData(double[] data) {
+//
+//    }
+//}

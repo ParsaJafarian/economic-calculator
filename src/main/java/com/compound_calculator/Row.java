6eb969970905b2b3eb27042e19c6f676b733a261
Parsa Jafarian <2234400@edu.vaniercollege.qc.ca>
2023-10-19 17:43:33 -0400

    Make tableview and prepare for pagination as next step

--- src/main/java/com/compound_calculator/Row.java
+++ src/main/java/com/compound_calculator/Row.java
@@ -1,2 +1,26 @@
-package com.compound_calculator;public class Row {
+package com.compound_calculator;
+
+public class Row {
+    private int time;
+    private double capital;
+    public Row(int time, double capital) {
+        this.time = time;
+        this.capital = capital;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public double getCapital() {
+        return capital;
+    }
+
+    public void setCapital(double capital) {
+        this.capital = capital;
+    }
 }
--- src/main/java/com/compound_calculator/Table.java
+++ src/main/java/com/compound_calculator/Table.java
@@ -1,2 +1,55 @@
-package com.compound_calculator;public class Table {
+package com.compound_calculator;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Button;
+import javafx.scene.control.Pagination;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class Table extends TableView{
+
+//    private final int itemsPerPage = 5; // Define the number of items per page
+//    private ObservableList<String> data = FXCollections.observableArrayList(); // Your data here
+
+    public Table() {
+        super();
+        TableColumn<Integer,String> timeCol = new TableColumn<>("Time (years)");
+        timeCol.setCellValueFactory(new PropertyValueFactory<>("time"));
+
+        TableColumn<Double, String> capitalCol = new TableColumn<>("Capital ($)");
+        capitalCol.setCellValueFactory(new PropertyValueFactory<>("capital"));
+
+        this.getColumns().add(timeCol);
+        this.getColumns().add(capitalCol);
+
+        this.getItems().add(new Row(0, 0.0));
+
+        this.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+
+
+        //@TODO: Add pagination
+//        Pagination pagination = new Pagination((int) Math.ceil((double) data.size() / itemsPerPage), 0);
+//        pagination.setPageFactory(pageIndex -> {
+//            this.setItems(getItemsForPage(pageIndex));
+//            return new VBox(this);
+//        });
+//
+//        Button nextButton = new Button("Next");
+//        nextButton.setOnAction(e -> {
+//            int nextPage = pagination.getCurrentPageIndex() + 1;
+//            if (nextPage < pagination.getPageCount()) {
+//                pagination.setCurrentPageIndex(nextPage);
+//            }
+//        });
+
+    }
+
+//    private ObservableList<String> getItemsForPage(int pageIndex) {
+//        int startIndex = pageIndex * itemsPerPage;
+//        int endIndex = Math.min(startIndex + itemsPerPage, data.size());
+//        return FXCollections.observableArrayList(data.subList(startIndex, endIndex));
+//    }
 }

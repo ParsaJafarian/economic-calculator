a6dc651c61b17716bf2b0fe875af657183ea2dc8
ParsaJafarian <108084316+Parsilius@users.noreply.github.com>
2023-10-31 19:16:17 -0400

    Try to merge gracefully

--- src/main/java/com/compound_calculator/Main.java
+++ src/main/java/com/compound_calculator/Main.java
@@ -14,36 +14,8 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-//        BorderPane root = new BorderPane();
-//
-//        HBox hBox = new HBox();
-//        hBox.setPadding(new Insets(20));
-//        VBox vBox = new VBox(20);
-//
-//        Table table = new Table();
-//
-//        vBox.getChildren().addAll(new InputSection(table), table);
-//        hBox.getChildren().addAll(vBox);
-
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("index.fxml"));
-
-
-<<<<<<< HEAD
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("index.fxml"));
         Scene scene = new Scene(loader.load(), 320, 240);
-=======
-        vBox.getChildren().addAll(new InputSection(table), table);
-        hBox.getChildren().addAll(vBox);
-
-        root.setTop(new Bar());
-        root.setCenter(hBox);
-
-        //provide ArrayList<Double> to this Graph object
-
-        //Graph graph= new Graph();
-        //root.setRight(new );
-
-        Scene scene = new Scene(root, 320, 240);
->>>>>>> 85de1db58eb7e3c96c0bf9f02dbf47abb2ef13c4
         stage.setTitle("Compound Interest Calculator");
         stage.setScene(scene);
         stage.show();

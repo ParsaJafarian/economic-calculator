c73e9a64009f93df43422e0fbd2cc584d093fa55
ParsaJafarian <108084316+Parsilius@users.noreply.github.com>
2023-11-06 11:21:33 -0500

    Reorganize code in order to adapt to TableController's creation

--- src/main/java/com/compound_calculator/Controller.java
+++ src/main/java/com/compound_calculator/Controller.java
@@ -1,8 +1,12 @@
 package com.compound_calculator;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import com.compound_calculator.TableController.Row;
+
 /**
  * Controller class for the main view
  */
@@ -31,6 +35,9 @@ public class Controller {
     private TextField yearsField;
     @FXML
     private Button excelBtn;
+    @FXML
+    private Pagination pagination;
+    private TableController tableUtil;
 
     /**
      * Initialize the view by adding the options to the frequency combo box,
@@ -43,14 +50,20 @@ public class Controller {
         freqBox.getItems().addAll("Monthly", "Quarterly", "Semi-annually", "Yearly");
         freqBox.getSelectionModel().selectFirst();
 
+        // Initialize the table & make text fields numeric
+        tableUtil = new TableController(this.table, pagination);
+        InputSectionUtils.makeTextFieldsNumeric(inputSection);
+
         // Add listeners to the buttons
         calcBtn.setOnAction(e -> calculate());
         clrBtn.setOnAction(e -> clear());
-        excelBtn.setOnAction(e -> TableUtils.exportToExcel(table));
+        excelBtn.setOnAction(e -> tableUtil.exportToExcel());
+    }
 
-        // Initialize the table & make text fields numeric
-        TableUtils.initializeTable(table);
-        InputSectionUtils.makeTextFieldsNumeric(inputSection);
+    private void updateTableVisibility(boolean visible){
+        table.setVisible(visible);
+        excelBtn.setVisible(visible);
+        pagination.setVisible(visible);
     }
 
     /**
@@ -59,8 +72,8 @@ public class Controller {
      */
     private void clear() {
         //Set table to invisible and select the first option in the frequency combo box
-        table.setVisible(false);
-        excelBtn.setVisible(false);
+        table.getItems().clear();
+        updateTableVisibility(false);
         freqBox.getSelectionModel().selectFirst();
         //Clear all text fields
         inputSection.getChildren().forEach(n -> {
@@ -72,7 +85,6 @@ public class Controller {
     /**
      * Calculate the compound interest and display the results in the table.
      */
-    @FXML
     public void calculate() {
 
         //If the input is invalid, display an error message and stop the calculation process
@@ -86,14 +98,13 @@ public class Controller {
         }
 
         //Get the input from the text fields
-        double initInv = Double.parseDouble(initInvField.getText());
-        double yearlyAddition = Double.parseDouble(yearlyAdditionField.getText());
-        double interest = Double.parseDouble(interestField.getText()) / 100;
-        int years = Integer.parseInt(yearsField.getText());
-
+        final double initInv = Double.parseDouble(initInvField.getText());
+        final double yearlyAddition = Double.parseDouble(yearlyAdditionField.getText());
+        final double interest = Double.parseDouble(interestField.getText()) / 100;
+        final int years = Integer.parseInt(yearsField.getText());
 
         //Get the frequency from the combo box
-        int freq = switch (freqBox.getValue()) {
+        final int freq = switch (freqBox.getValue()) {
             case "Monthly" -> 12;
             case "Quarterly" -> 4;
             case "Semi-annually" -> 2;
@@ -102,24 +113,24 @@ public class Controller {
         };
 
         //Initialize the data array (note: the size is years + 1 because the first row is the initial investment)
-        Row[] data = new Row[years + 1];
+        ObservableList<Row> data = FXCollections.observableArrayList();
         //last is a variable that points to the last row inserted into the data array
         Row last = new Row(0, initInv);
         //Set the first row to the initial investment
-        data[0] = last;
+        data.add(last);
 
         //Loop through the years and calculate the compound interest
         for (int i = 1; i < years * freq + 1; i++) {
-            Row curr = new Row(i / freq, last.getCapital() * (1 + interest / freq) + yearlyAddition / freq);
+            final int time = i / freq;
+            final double capital = last.getCapital() * (1 + interest / freq) + yearlyAddition / freq;
+            Row curr = new Row(time, capital);
             //If the current amount of time is a multiple of the frequency, add the row to the data array
             if (i % freq == 0)
-                data[i / freq] = curr;
+                data.add(curr);
             //Set last to the current row
             last = curr;
         }
-        //Set the data in the table to the data array and make the table visible
-        TableUtils.setTabularData(table, data);
-        table.setVisible(true);
-        excelBtn.setVisible(true);
+        tableUtil.setData(data);
+        updateTableVisibility(true);
     }
 }
--- src/main/java/com/compound_calculator/Main.java
+++ src/main/java/com/compound_calculator/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(Main.class.getResource("index.fxml"));
-        Scene scene = new Scene(loader.load(), 600, 600);
+        Scene scene = new Scene(loader.load(), 600, 700);
         stage.setTitle("Compound Interest Calculator");
         stage.setScene(scene);
         stage.show();

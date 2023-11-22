b2d36f4491502a18ed41edf63a074614d8ab491d
ParsaJafarian <108084316+Parsilius@users.noreply.github.com>
2023-11-22 11:08:04 -0500

    Adjust to changes in com.compound_calculator

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,4 +7,8 @@ module com.compound_calculator {
 
     opens com.compound_calculator to javafx.fxml;
     exports com.compound_calculator;
+    exports com.compound_calculator.forms;
+    opens com.compound_calculator.forms to javafx.fxml;
+    exports com.compound_calculator.utils;
+    opens com.compound_calculator.utils to javafx.fxml;
 }
--- src/test/java/com/compound_calculator/ControllerTest.java
+++ src/test/java/com/compound_calculator/ControllerTest.java
@@ -1,6 +1,5 @@
 package com.compound_calculator;
 
-import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -9,7 +8,6 @@ class ControllerTest {
 
     @Test
     void initialize() throws IOException {
-        Main main = new Main();
-        main.start(new Stage());
+
     }
 }

5ed6fc067e7fcc1bc9cd2396cd2d1b303bae36fa
ParsaJafarian <108084316+Parsilius@users.noreply.github.com>
2023-11-22 11:05:37 -0500

    Create AnnuityForm and InflationForm and implement methods from Form

--- src/main/java/com/compound_calculator/forms/AnnuityForm.java
+++ src/main/java/com/compound_calculator/forms/AnnuityForm.java
@@ -1,2 +1,34 @@
-package com.compound_calculator.forms;public class AnnuityForm {
+package com.compound_calculator.forms;
+
+import com.compound_calculator.Row;
+import javafx.collections.ObservableList;
+import javafx.scene.layout.GridPane;
+
+public class AnnuityForm extends Form{
+    public AnnuityForm(GridPane gridPane) {
+        super(gridPane);
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean validFields() {
+        return false;
+    }
+
+    @Override
+    public ObservableList<Row> getData() {
+        return null;
+    }
+
+    private void computePresentValueAnnuity(ObservableList<Row> data, int nbPeriods, double annuityPayment, double yieldToMaturity){
+        for(int i=1; i<= nbPeriods; i++) {
+            double compoundingFactor = 1.0f - 1.0f / Math.pow(1.0f + i, i);
+            double presentValue = annuityPayment / yieldToMaturity * compoundingFactor;
+            data.add(new Row(i, presentValue));
+        }
+    }
 }
--- src/main/java/com/compound_calculator/forms/InflationForm.java
+++ src/main/java/com/compound_calculator/forms/InflationForm.java
@@ -1,2 +1,36 @@
-package com.compound_calculator.forms;public class InflationForm {
+package com.compound_calculator.forms;
+
+import com.compound_calculator.Row;
+import javafx.collections.ObservableList;
+import javafx.scene.layout.GridPane;
+
+public class InflationForm extends Form{
+    public InflationForm(GridPane gridPane) {
+        super(gridPane);
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean validFields() {
+        return false;
+    }
+
+    @Override
+    public ObservableList<Row> getData() {
+        return null;
+    }
+
+    private double computeInflationRate(double currentCPI, double previousCPI){
+        return (currentCPI-previousCPI)/previousCPI *100.0f;
+    }
+    private double computeYearlyInflationRate(double currentCPI, double previousCPI, double currentYear, double previousYear){
+        double inflationRate= computeInflationRate(currentCPI, previousCPI);
+        double deltaYears= currentYear-previousYear;
+        double v = inflationRate / deltaYears;
+        return v;
+    }
 }

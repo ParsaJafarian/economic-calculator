fa03348f56c08688c8f3c0858150c560979413a8
ParsaJafarian <108084316+Parsilius@users.noreply.github.com>
2023-11-27 20:16:19 -0500

    Fix Bug

--- src/main/java/com/compound_calculator/util/MenuBarUtils.java
+++ src/main/java/com/compound_calculator/util/MenuBarUtils.java
@@ -67,7 +67,7 @@ public class MenuBarUtils {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("About");
             alert.setHeaderText("Compound Interest Calculator");
-            alert.setContentText("Created by Mark Rudko && Parsa Jafarian.");
+            alert.setContentText("Created by Parsa Jafarian & Mark Rudko");
             alert.showAndWait();
         });
 
